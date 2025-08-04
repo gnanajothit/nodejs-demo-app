edfc7be623e7afb541e21d262472d9713c665d3b
Gnana_Jothi <Gnana_Jothi@gmail.com>
2025-08-04 06:39:51 +0000

    Inital commit

---
+++ app.js
@@ -0,0 +1,10 @@
+    // app.js
+const http = require('http');
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello from GitHub Actions CI/CD!');
+});
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
